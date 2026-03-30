618241cda4add741f1dd9e46378e23335031e13b
EHotwagner <ehotwagner@gmail.com>
2026-03-30 21:00:20 +0200

    auto-sync 2026-03-30T21:00:20+02:00

--- transient/history.el
+++ transient/history.el
@@ -1,6 +1,8 @@
 ((docker nil)
  (docker-container-attach nil)
  (docker-container-restart nil)
+ (docker-container-rm nil)
  (docker-container-start nil)
+ (docker-container-stop nil)
  (docker-image-run
   ("-t" "--rm" "-i")))
